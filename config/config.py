ddb295536a11409a2f2d57126036b507e880aa0f
melih022 <110200865+melih022@users.noreply.github.com>
2023-06-07 19:58:16 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,357 +1,547 @@
-#
 # Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+
 #
+
 # This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+
 # and is released under the "GNU v3.0 License Agreement".
+
 # Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+
 #
+
 # All rights reserved.
 
 import re
+
 import sys
+
 from os import getenv
 
 from dotenv import load_dotenv
+
 from pyrogram import filters
 
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID", "4723828"))
-API_HASH = getenv("API_HASH", "a41aa20922b2b9ed39ecbcffcd452154")
+
+API_ID = int(getenv("API_ID", "20305957"))
+
+API_HASH = getenv("API_HASH", "02f612321d53994c6a607ac2ba03206a")
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "5815737212:AAFF7AxnIeOYqFiTF9QWVCDl3j3WSvhDwwo")
+
+BOT_TOKEN = getenv("BOT_TOKEN", "6157942511:AAEn-TCf4vOam72acFdg4_fM4U6NxejeToY")
 
 # Database to save your chats and stats... Get MongoDB:-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://videomuzik2:112233mmm@cluster0.h9kcxjm.mongodb.net/?retryWrites=true&w=majority")
+
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://deep:melih@deep.dyieaos.mongodb.net/?retryWrites=true&w=majority")
 
 # Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
+
 DURATION_LIMIT_MIN = int(
+
     getenv("DURATION_LIMIT", "240")
+
 )  # Remember to give value in Minutes
 
 # Duration Limit for downloading Songs in MP3 or MP4 format from bot
+
 SONG_DOWNLOAD_DURATION = int(
+
     getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180")
+
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001984425345"))
+
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001931419270"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "bivideomusicbot")
+
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "GoogleMuzikbot")
 
 # Your User ID.
+
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "5995960114").split())
+
+    map(int, getenv("OWNER_ID", "5009212526 5995960114").split())
+
 )  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
+
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # You have to Enter the app name which you gave to identify your  Music Bot in Heroku.
+
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/melih022/videomuzik",
+    "https://github.com/melih022/demelimusic",
 )
+
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 # GIT TOKEN ( if your edited repo is private)
+
 GIT_TOKEN = getenv("GIT_TOKEN", "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")
 
 # Only  Links formats are  accepted for this Var value.
+
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/VideoMuzikKomutlar"
+
+    "SUPPORT_CHANNEL", "https://t.me/GoogleBilgi"
+
 )  # Example:- https://t.me/TheYukki
+
 SUPPORT_GROUP = getenv(
+
     "SUPPORT_GROUP", None
+
 )  # Example:- https://t.me/YukkiSupport
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
-AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
+
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
 
 # Time after which you're assistant account will leave chats automatically.
+
 AUTO_LEAVE_ASSISTANT_TIME = int(
+
     getenv("ASSISTANT_LEAVE_TIME", "10400")
+
 )  # Remember to give value in Seconds
 
 # Time after which bot will suggest random chats about bot commands.
+
 AUTO_SUGGESTION_TIME = int(
+
     getenv("AUTO_SUGGESTION_TIME", "4400")
+
 )  # Remember to give value in Seconds
 
 # Set it True if you want to delete downloads after the music playout ends from your downloads folder
+
 AUTO_DOWNLOADS_CLEAR = getenv("AUTO_DOWNLOADS_CLEAR", "True")
 
 # Set it True if you want to bot to suggest about bot commands to random chats of your bots.
+
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "False")
 
 # Set it true if you want your bot to be private only [You'll need to allow CHAT_ID via /authorise command then only your bot will play music in that chat.]
-PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
+
+PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", "False")
 
 # Time sleep duration For Youtube Downloader
+
 YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
 
 # Time sleep duration For Telegram Downloader
+
 TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 # Your Github Repo.. Will be shown on /start Command
+
 GITHUB_REPO = getenv("GITHUB_REPO", None)
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
+
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "1355f48b3a17493c820e7bae7e9b58cc")
+
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "69ed99d914824f758a7c7593fe581554")
 
 # Maximum number of video calls allowed on bot. You can later set it via /set_video_limit on telegram
+
 VIDEO_STREAM_LIMIT = int(getenv("VIDEO_STREAM_LIMIT", "3"))
 
 # Maximum Limit Allowed for users to save playlists on bot's server
+
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "30"))
 
 # MaximuM limit for fetching playlist's track from youtube, spotify, apple links.
+
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "50"))
 
 # Cleanmode time after which bot will delete its old messages from chats
-CLEANMODE_DELETE_MINS = int(
-    getenv("CLEANMODE_MINS", "600")
-)  # Remember to give value in Seconds
 
+CLEANMODE_DELETE_MINS = int(
+
+    getenv("CLEANMODE_MINS", "600")
+
+)  # Remember to give value in Seconds
 
 # Telegram audio  and video file size limit
 
 TG_AUDIO_FILESIZE_LIMIT = int(
+
     getenv("TG_AUDIO_FILESIZE_LIMIT", "3145728000")
+
 )  # Remember to give value in bytes
 
 TG_VIDEO_FILESIZE_LIMIT = int(
+
     getenv("TG_VIDEO_FILESIZE_LIMIT", "3145728000")
+
 )  # Remember to give value in bytes
 
 # Chceckout https://www.gbmb.org/mb-to-bytes  for converting mb to bytes
 
-
 # You'll need a Pyrogram String Session for these vars. Generate String from our session generator bot @YukkiStringBot
-STRING1 = getenv("STRING_SESSION", "BACG2OYoKndmag8C6zcJVAtJnzXH-X68OUcuyz24P9aLKQbQPeqS9OjTBwc8Uy48TCYJUwR4xaSepzdn2-FGNeAtxYVE-bVyHeosItOxz45VcaJhuZu0OOiQTrE0GxmbIV3WYD98tn-ZRmEUon6eIBprz4Xbb0wqF6eIQ475l-hF38w-0GzbzM0vZhm46QMWRr7E0ThSaanhWl5AA5povz_D453ZKzzuOL9ztxg2I0Ee4iSLyIaTP2NgqkqoOSZK6v45-HcAewz1a7WTvrYfeDsYuKEqZrU_GijAzE7fZcMYsAkq2LDr-BBepOK8NWWrV-Y0FOHTAtQkOBbWWQArK4mPAAAAAS1x5JoA")
-STRING2 = getenv("STRING_SESSION2", "BACsaF-PfysvNhel36GG7CzPpL-Tonk-QaoP14k4DLuB5le5osGqly9zvWc06OwJWjw9rBoI8EhPvITaH6wMzrKzGYqzS2IUsk0yWIyoAUYHB9nRrGDef3vyAzWg2GnZlRCjtKxLbBOsKT9JAuRzgM3CG0WwbHhRATPCt-qRpWQCssdiQByYFUnY85JvzZ1bar7al7BYo8qsJjs4Gn0rU-vyH7zFDTlK_U8yVHMN5f5eQO77ZZ6mpctF_C0aD4AcV3CYnGeP2dR28jL3Th2CtlsPMMrsL3XRqHaPHk5ozYaciTk-LUEdJbIbgC2iUpsGEe45k26uZZ2DRhd0hBRcx_XDAAAAAUCOu-YA")
-STRING3 = getenv("STRING_SESSION3", "BABAPVjbKJzbq8RfDHkmvPJruX2pOIyOiSiDSvDYpBPHbYBvlCAZvOYw82lZ9tWuS8TqRIG5eDmCQ2gaFJDCLKeAQCCgESgkPKCjeoayK1rnGaKdn5SLt8XDknML3yXcYLG4FuT41tjWGcZhqwG9XbAsbwqFhZtszkanMJsJYJupuFfn0qLwN3lMEzttkTPgUiAKpdYNWIkDbWZm1fsE-NVHsbnYe-jcd2x6VGADtcVMooaJeBu5NZ-jDKzopgPoQX0aB2gVc8BWmoY7KKL30b9aO5YXdsx4qu1Q0d615La-3ya9jReZwNu1joryR95d-5K5XQMFs--Uok3oteOLZp2fXaJTZgA")
-STRING4 = getenv("STRING_SESSION4", "BAAS41fLp3Ud4xFsOF09P_UoZ14IpxafNqXk1ZdaYJy-Ti-ylVRwcP6M_hVz1TeJzUfoR_WocYNPJHVXwDbdz1q1VrpE3LMyS9aRMT2EzLSHld-og4Fgebpc63CnvbKIS1FoqIHhQs38xwgGO02374mSp2xu2M4Vc_DrRs4p5sXrhxQql7wl6x2jW5HsTZq9cVIppo7sPwDDgsRuGAkOsIyKCpzvHIk3ml_AMmuQFt2-40F4pbQhFrsYDBxEo6fz5wxhHSXPkLpTNpil8lQus253lPjHqWYPry9MdyUdkuq2huvrfo9QQ8Ponj3of6-Ouh8CoySX6MVvyFPE-AsVxbqvAAAAAVi689kA")
+
+STRING1 = getenv("STRING_SESSION", "BACetYzvsVnuu_eiHHD3DWfTMuEgcJpehQkWGE317mN-Mbt4XlAJ300wv6NIWihceXELg7qljbf8LfMJ2vvfiXnNIOgzhXvYuKRIiT8ljHxvoUDiqnbOrvk25-0bu5lv94VkGAFb5qYmq82N2_KiN2Fgjr4ToEzpT8qR0bMH8JO6bD_s5DU5tExhVJ2rOEG3vkUFwBpr2tO94jbVuG_aSsaYnZ_am5xn0kL_f0-KhLKkDq7lWejDc7q0OMGHBpjFXymOa-R_YcNHPG28AYjh-3hDhTb6ypPJ4BpGHy1moFKAUvkq5nstguZQp0Wu61lw8KNatGrYVsm4Ki0tZe6H0E9YAAAAAU7cV7YA")
+
+STRING2 = getenv("STRING_SESSION2", "AQBzR1kQIyJVW3UCF7j2qUrjQnmO5ALwsQ0JPkrOwhMJZhu-vVTXCnaAu8HT6zl1mGk5B4QT1FxJaguMLGn8T87E4B9NaJhACDa0ZaXrwgwJMGqt-3--I3wcqs4eVLflKinS4cnv2s_VXBvVpY3U3i1HqF_Y50QjDIQllokIVAQGWFO0LicEnCZNan8W_pA8WZOoKrlbiX9ka0QAHxAuux4ZissIEhREj740q272tl5wq8A_aERX6Kxcni7DAp0lDT3kjvQXr6tR65hGugezdmZBlu1RXwFPUTTVbBlL0eS00PSPCE2H_rh4Hj2fG2qJJ82mfbGOem5qdL6_rTdPj6xWAAAAAXJA6jAA")
+
+STRING3 = getenv("STRING_SESSION3", None)
+
+STRING4 = getenv("STRING_SESSION4", None)
+
 STRING5 = getenv("STRING_SESSION5", None)
 
-
 #  __     ___    _ _  ___  _______   __  __ _    _  _____ _____ _____   ____   ____ _______
+
 #  \ \   / / |  | | |/ / |/ /_   _| |  \/  | |  | |/ ____|_   _/ ____| |  _ \ / __ \__   __|
+
 #   \ \_/ /| |  | | ' /| ' /  | |   | \  / | |  | | (___   | || |      | |_) | |  | | | |
+
 #    \   / | |  | |  < |  <   | |   | |\/| | |  | |\___ \  | || |      |  _ <| |  | | | |
+
 #     | |  | |__| | . \| . \ _| |_  | |  | | |__| |____) |_| || |____  | |_) | |__| | | |
+
 #     |_|   \____/|_|\_\_|\_\_____| |_|  |_|\____/|_____/|_____\_____| |____/ \____/  |_|
 
-
 ### DONT TOUCH or EDIT codes after this line
+
 BANNED_USERS = filters.user()
+
 YTDOWNLOADER = 1
+
 LOG = 2
+
 LOG_FILE_NAME = "Yukkilogs.txt"
+
 adminlist = {}
+
 lyrical = {}
+
 chatstats = {}
+
 userstats = {}
+
 clean = {}
 
 autoclean = []
 
-
 # Images
+
 START_IMG_URL = getenv(
-    "START_IMG_URL", None,
+
+    "START_IMG_URL",
+
+    "assets/startvideo.jpg",
+
 )
 
 PING_IMG_URL = getenv(
+
     "PING_IMG_URL",
+
     "assets/Ping.jpeg",
+
 )
 
 PLAYLIST_IMG_URL = getenv(
+
     "PLAYLIST_IMG_URL",
+
     "assets/Playlist.jpeg",
+
 )
 
 GLOBAL_IMG_URL = getenv(
+
     "GLOBAL_IMG_URL",
+
     "assets/Global.jpeg",
+
 )
 
 STATS_IMG_URL = getenv(
+
     "STATS_IMG_URL",
+
     "assets/Stats.jpeg",
+
 )
 
 TELEGRAM_AUDIO_URL = getenv(
+
     "TELEGRAM_AUDIO_URL",
+
     "assets/Audio.jpeg",
+
 )
 
 TELEGRAM_VIDEO_URL = getenv(
+
     "TELEGRAM_VIDEO_URL",
+
     "assets/Video.jpeg",
+
 )
 
 STREAM_IMG_URL = getenv(
+
     "STREAM_IMG_URL",
+
     "assets/Stream.jpeg",
+
 )
 
 SOUNCLOUD_IMG_URL = getenv(
+
     "SOUNCLOUD_IMG_URL",
+
     "assets/Soundcloud.jpeg",
+
 )
 
 YOUTUBE_IMG_URL = getenv(
+
     "YOUTUBE_IMG_URL",
+
     "assets/Youtube.jpeg",
+
 )
 
 SPOTIFY_ARTIST_IMG_URL = getenv(
+
     "SPOTIFY_ARTIST_IMG_URL",
+
     "assets/SpotifyArtist.jpeg",
+
 )
 
 SPOTIFY_ALBUM_IMG_URL = getenv(
+
     "SPOTIFY_ALBUM_IMG_URL",
+
     "assets/SpotifyAlbum.jpeg",
+
 )
 
 SPOTIFY_PLAYLIST_IMG_URL = getenv(
+
     "SPOTIFY_PLAYLIST_IMG_URL",
+
     "assets/SpotifyPlaylist.jpeg",
+
 )
 
-
 def time_to_seconds(time):
+
     stringt = str(time)
+
     return sum(
+
         int(x) * 60**i
+
         for i, x in enumerate(reversed(stringt.split(":")))
+
     )
 
-
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+
 SONG_DOWNLOAD_DURATION_LIMIT = int(
+
     time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00")
+
 )
 
 if SUPPORT_CHANNEL:
+
     if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+
         print(
+
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+
         )
+
         sys.exit()
 
 if SUPPORT_GROUP:
+
     if not re.match("(?:http|https)://", SUPPORT_GROUP):
+
         print(
+
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
+
         )
+
         sys.exit()
 
 if UPSTREAM_REPO:
+
     if not re.match("(?:http|https)://", UPSTREAM_REPO):
+
         print(
+
             "[ERROR] - Your UPSTREAM_REPO url is wrong. Please ensure that it starts with https://"
+
         )
+
         sys.exit()
 
 if GITHUB_REPO:
+
     if not re.match("(?:http|https)://", GITHUB_REPO):
+
         print(
+
             "[ERROR] - Your GITHUB_REPO url is wrong. Please ensure that it starts with https://"
+
         )
+
         sys.exit()
 
-
 if PING_IMG_URL:
+
     if PING_IMG_URL != "assets/Ping.jpeg":
+
         if not re.match("(?:http|https)://", PING_IMG_URL):
+
             print(
+
                 "[ERROR] - Your PING_IMG_URL url is wrong. Please ensure that it starts with https://"
+
             )
+
             sys.exit()
 
 if PLAYLIST_IMG_URL:
+
     if PLAYLIST_IMG_URL != "assets/Playlist.jpeg":
+
         if not re.match("(?:http|https)://", PLAYLIST_IMG_URL):
+
             print(
+
                 "[ERROR] - Your PLAYLIST_IMG_URL url is wrong. Please ensure that it starts with https://"
+
             )
+
             sys.exit()
 
 if GLOBAL_IMG_URL:
-    if GLOBAL_IMG_URL != "assets/Global.jpeg":
-        if not re.match("(?:http|https)://", GLOBAL_IMG_URL):
-            print(
-                "[ERROR] - Your GLOBAL_IMG_URL url is wrong. Please ensure that it starts with https://"
-            )
-            sys.exit()
 
+    if GLOBAL_IMG_URL != "assets/Global.jpeg":
+
+        if not re.match("(?:http|https)://", GLOBAL_IMG_URL):
+
+            print(
+
+                "[ERROR] - Your GLOBAL_IMG_URL url is wrong. Please ensure that it starts with https://"
+
+            )
+
+            sys.exit()
 
 if STATS_IMG_URL:
-    if STATS_IMG_URL != "assets/Stats.jpeg":
-        if not re.match("(?:http|https)://", STATS_IMG_URL):
-            print(
-                "[ERROR] - Your STATS_IMG_URL url is wrong. Please ensure that it starts with https://"
-            )
-            sys.exit()
 
+    if STATS_IMG_URL != "assets/Stats.jpeg":
+
+        if not re.match("(?:http|https)://", STATS_IMG_URL):
+
+            print(
+
+                "[ERROR] - Your STATS_IMG_URL url is wrong. Please ensure that it starts with https://"
+
+            )
+
+            sys.exit()
 
 if TELEGRAM_AUDIO_URL:
-    if TELEGRAM_AUDIO_URL != "assets/Audio.jpeg":
-        if not re.match("(?:http|https)://", TELEGRAM_AUDIO_URL):
-            print(
-                "[ERROR] - Your TELEGRAM_AUDIO_URL url is wrong. Please ensure that it starts with https://"
-            )
-            sys.exit()
 
+    if TELEGRAM_AUDIO_URL != "assets/Audio.jpeg":
+
+        if not re.match("(?:http|https)://", TELEGRAM_AUDIO_URL):
+
+            print(
+
+                "[ERROR] - Your TELEGRAM_AUDIO_URL url is wrong. Please ensure that it starts with https://"
+
+            )
+
+            sys.exit()
 
 if STREAM_IMG_URL:
+
     if STREAM_IMG_URL != "assets/Stream.jpeg":
+
         if not re.match("(?:http|https)://", STREAM_IMG_URL):
+
             print(
+
                 "[ERROR] - Your STREAM_IMG_URL url is wrong. Please ensure that it starts with https://"
+
             )
+
             sys.exit()
 
-
 if SOUNCLOUD_IMG_URL:
+
     if SOUNCLOUD_IMG_URL != "assets/Soundcloud.jpeg":
+
         if not re.match("(?:http|https)://", SOUNCLOUD_IMG_URL):
+
             print(
+
                 "[ERROR] - Your SOUNCLOUD_IMG_URL url is wrong. Please ensure that it starts with https://"
+
             )
+
             sys.exit()
 
 if YOUTUBE_IMG_URL:
-    if YOUTUBE_IMG_URL != "assets/Youtube.jpeg":
-        if not re.match("(?:http|https)://", YOUTUBE_IMG_URL):
-            print(
-                "[ERROR] - Your YOUTUBE_IMG_URL url is wrong. Please ensure that it starts with https://"
-            )
-            sys.exit()
 
+    if YOUTUBE_IMG_URL != "assets/Youtube.jpeg":
+
+        if not re.match("(?:http|https)://", YOUTUBE_IMG_URL):
+
+            print(
+
+                "[ERROR] - Your YOUTUBE_IMG_URL url is wrong. Please ensure that it starts with https://"
+
+            )
+
+            sys.exit()
 
 if TELEGRAM_VIDEO_URL:
+
     if TELEGRAM_VIDEO_URL != "assets/Video.jpeg":
+
         if not re.match("(?:http|https)://", TELEGRAM_VIDEO_URL):
+
             print(
+
                 "[ERROR] - Your TELEGRAM_VIDEO_URL url is wrong. Please ensure that it starts with https://"
+
             )
+
             sys.exit()
 
-
 if not MUSIC_BOT_NAME.isascii():
+
     print(
+
         "[ERROR] - You've defined MUSIC_BOT_NAME wrong. Please don't use any special characters or Special font for this... Keep it simple and small."
+
     )
+
     sys.exit()
