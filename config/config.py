0872dfdba72b83b7178427161e5b8d612b64e76e
melih022 <110200865+melih022@users.noreply.github.com>
2023-08-22 01:04:44 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -34,7 +34,7 @@ API_HASH = getenv("API_HASH", "02f612321d53994c6a607ac2ba03206a")
 
 ## Get it from @Botfather in Telegram.
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6157942511:AAEn-TCf4vOam72acFdg4_fM4U6NxejeToY")
+BOT_TOKEN = getenv("BOT_TOKEN", "6157942511:AAE_O2oiUfrKV0D4h2Wgz7AqL48OFPIbxkI")
 
 # Database to save your chats and stats... Get MongoDB:-  https://telegra.ph/How-To-get-Mongodb-URI-04-06
 
