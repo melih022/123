03f8e994f6fc76acb976ec2a51b9a8b8c834b391
melih022 <110200865+melih022@users.noreply.github.com>
2023-08-28 18:30:50 +0300

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -19,7 +19,7 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-**Miss LOG**
+**Google Log**
 
 **Grup:** {message.chat.title} [`{message.chat.id}`]
 
