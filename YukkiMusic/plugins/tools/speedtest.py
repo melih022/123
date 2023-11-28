0f298b15254528574e48598f8348a1b1e1a5a1cc
melih022 <110200865+melih022@users.noreply.github.com>
2023-11-28 22:12:42 +0300

    Update speedtest.py

--- YukkiMusic/plugins/tools/speedtest.py
+++ YukkiMusic/plugins/tools/speedtest.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import asyncio
 import speedtest
 from pyrogram import filters
@@ -14,45 +5,46 @@ from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.misc import SUDOERS
 
-from pyrogram import filters
-from pyrogram.types import Message
-
 # Commands
 SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
 
 
-
-def testspeed(m, _):
+def testspeed(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit_text(_["server_12"])
+        m = m.edit("İndirme Hız Testi Çalıştırılıyor")
         test.download()
-        m = m.edit_text(_["server_13"])
+        m = m.edit("Yükleme Hız Testini Çalıştırılıyor")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = m.edit_text(_["server_14"])
+        m = m.edit("Hız Sonuçlarını Paylaşmak İçin Hazır")
     except Exception as e:
-        return m.edit_text(f"<code>{e}</code>")
+        return m.edit(e)
     return result
 
 
-@app.on_message(filters.command(["speedtest", "spt"]) & SUDOERS)
-@language
-async def speedtest_function(client, message: Message, _):
-    m = await message.reply_text(_["server_11"])
+@app.on_message(filters.command(SPEEDTEST_COMMAND) & SUDOERS)
+async def speedtest_function(client, message):
+    m = await message.reply_text("Hız Testi")
     loop = asyncio.get_event_loop()
-    result = await loop.run_in_executor(None, testspeed, m, _)
-    output = _["server_15"].format(
-        result["client"]["isp"],
-        result["client"]["country"],
-        result["server"]["name"],
-        result["server"]["country"],
-        result["server"]["cc"],
-        result["server"]["sponsor"],
-        result["server"]["latency"],
-        result["ping"],
+    result = await loop.run_in_executor(None, testspeed, m)
+    output = f"""**HızTestı Sonuc**
+    
+<u>**Client:**</u>
+**__ISP:__** {result['client']['isp']}
+**__Ülke:__** {result['client']['country']}
+  
+<u>**Server:**</u>
+**__İsim:__** {result['server']['name']}
+**__Ülke:__** {result['server']['country']}, {result['server']['cc']}
+**__Sponsor:__** {result['server']['sponsor']}
+**__Geçikme:__** {result['server']['latency']}  
+**__Ping:__** {result['ping']}"""
+    msg = await app.send_photo(
+        chat_id=message.chat.id, 
+        photo=result["share"], 
+        caption=output
     )
-    msg = await message.reply_photo(photo=result["share"], caption=output)
     await m.delete()
