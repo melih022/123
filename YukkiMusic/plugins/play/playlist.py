bdf709468b5325233a3a50b3cf98431996f1e845
melih022 <110200865+melih022@users.noreply.github.com>
2024-03-06 23:24:22 +0300

    Update playlist.py

--- YukkiMusic/plugins/play/playlist.py
+++ YukkiMusic/plugins/play/playlist.py
@@ -71,7 +71,7 @@ async def check_playlist(client, message: Message, _):
 @app.on_message(
     filters.command(DELETEPLAYLIST_COMMAND)
     & filters.group
-    & ~filters.edited
+    #& ~filters.edited
     & ~BANNED_USERS
 )
 @language
@@ -118,7 +118,7 @@ async def get_keyboard(_, user_id):
 @app.on_message(
     filters.command(DELETEPLAYLIST_COMMAND)
     & filters.private
-    & ~filters.edited
+    #& ~filters.edited
     & ~BANNED_USERS
 )
 @language
